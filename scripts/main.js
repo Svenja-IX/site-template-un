bbd6fcffb024e0351949fb2380401fd59e13deef
Axel <axel.abad0901@gmail.com>
2020-02-27 20:34:24 +0100

    initialisation des fichiers

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$('#inscription').click(function(){
+	
+
+	$('#white').fadeOut('3000');
+	$('#texteBlanc').html('ok boomer');
+	$('#texteBlanc').css('color', 'white', 'display');
+	$('#texteBlanc').appendChild('#bleu');
+	$('#texteBlanc').fadeOut('slow');
+	
+});
