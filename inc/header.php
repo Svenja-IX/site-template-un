50e01fb5f6804dc2c0bf129958f7edc24a96d2f7
Abad <axel.abad0901@gmail.com>
2020-03-26 22:29:41 +0100

    inc php header

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+    <!-- HEADER -->
+    <header>
+        <span>
+        <img src='images/alexion.png'>
+        </span>
+        <div id='index'>
+        <h1>About</h1>
+        <h1>Métier</h1>
+        <h1>Réalisation</h1>
+        <h1>Références</h1>
+        <h1>Blog</h1>
+        <h1>Contact</h1>
+        </div>
+        <div id="boutons">
+            <a href="#"><span id='buttonUn'><p id="inscription" type="button">S'INSCRIRE</p></span></a>
+            <a href="#"><span id='buttonDeux'><p id="connexion" type="button">SE CONNECTER</p></span></a>
+        </div>
+    </header>
+    <!-- END HEADER / MAIN -->
--- index.php
+++ index.php
@@ -8,25 +8,9 @@
     <link rel='StyleSheet' href='styles/style.css'>
 </head>
 <body>
-    <!-- HEADER -->
-    <header>
-        <span>
-        <img src='images/alexion.png'>
-        </span>
-        <div id='index'>
-        <h1>About</h1>
-        <h1>Métier</h1>
-        <h1>Réalisation</h1>
-        <h1>Références</h1>
-        <h1>Blog</h1>
-        <h1>Contact</h1>
-        </div>
-        <div id="boutons">
-            <a href="#"><span id='buttonUn'><p id="inscription" type="button">S'INSCRIRE</p></span></a>
-            <a href="#"><span id='buttonDeux'><p id="connexion" type="button">SE CONNECTER</p></span></a>
-        </div>
-    </header>
-    <!-- END HEADER / MAIN -->
+<?php
+ include 'inc/header.php';
+?>
     <main>
         <article class='white' class="blocSize">
             <h2>About me</h2>
